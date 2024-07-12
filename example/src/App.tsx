2e73e39fa95410b081ac70d1ea7aa7bfda890b92
yoav <yoav@agrifrn.com>
2024-07-12 10:20:19 +0300

    style: format code with Prettier

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,18 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-image-magnifier';
+import { StyleSheet, View } from 'react-native';
+import ImageMagnifier from 'react-native-image-magnifier';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+  // const [result, setResult] = React.useState<number | undefined>();
 
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
+  // React.useEffect(() => {
+  //   multiply(3, 7).then(setResult);
+  // }, []);
 
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <ImageMagnifier />
     </View>
   );
 }
---
+++ src/ImageMagnifier.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+// import { styles } from './styles'; // Assuming styles are defined in a separate file
+
+interface ImageMagnifierProps {
+  // Define props here if your component accepts any
+}
+
+const ImageMagnifier: React.FC<ImageMagnifierProps> = () => {
+  return (
+    <View>
+      <Text>Image Magnifier</Text>
+    </View>
+  );
+};
+
+export default ImageMagnifier;
